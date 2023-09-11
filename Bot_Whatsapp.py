46a722160b629c49664de39645124067b8165d06
Rwann Pabblo <93088075+RPabblo@users.noreply.github.com>
2023-09-11 16:04:42 -0300

    Update Bot_Whatsapp.py

--- Bot_Whatsapp.py
+++ Bot_Whatsapp.py
@@ -1,3 +1,8 @@
-import pywhatskit
+# Necessária instalação de biblioteca. Abra o terminal e digite: pip3 install pywhatkit
 
-pywhatskit.sendwhatsmessage("+5522123456789", "mensagem", 15, 10)
+import pywhatkit
+
+def mensagem():
+    pywhatkit.sendwhatmsg("+5599912345678", "Mensagem", 16, 4, 30)
+
+mensagem()
