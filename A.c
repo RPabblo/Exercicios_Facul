955e6f859212c255d7f49181ae17c36ba7687efa
Rwann Pabblo <93088075+RPabblo@users.noreply.github.com>
2023-09-21 15:13:15 -0300

    Update A.c

--- A.c
+++ A.c
@@ -5,7 +5,7 @@
 
 typedef struct lista {
     int valor;
-    struct Lista *proximo;
+    struct lista *proximo;
 } Lista;
 
 
@@ -14,7 +14,6 @@ Lista* criar_no(int valor)
     Lista *novo_no = malloc(sizeof(Lista));
     novo_no->valor = valor;
     novo_no->proximo = NULL;
-
     return novo_no;
 }
 
@@ -33,23 +32,53 @@ void inserir(Lista **head, int value)
 
 void inserir_ordenado(Lista **lista, int value)
 {
-    Lista *new_Listade = criar_no(value);
+    Lista *novo_no = criar_no(value);
 
-    if(*lista == NULL || (*lista)->valor >= new_Listade->valor) {
-        new_Listade->proximo = *lista;
-        *lista = new_Listade;
+    if(*lista == NULL || (*lista)->valor >= novo_no->valor) {
+        novo_no->proximo = *lista;
+        *lista = novo_no;
         return;
     }
 
     Lista *ant = *lista;
     Lista *prox = ant->proximo;
 
-    while(prox != NULL && prox->valor < new_Listade->valor) {
+    while(prox != NULL && prox->valor < novo_no->valor) {
         ant = ant->proximo;
         prox = prox->proximo;
     }
-    new_Listade->proximo = prox;
-    ant->proximo = new_Listade;
+    novo_no->proximo = prox;
+    ant->proximo = novo_no;
+}
+
+void append(Lista** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    Lista* new_node = (Lista*)malloc(sizeof(Lista));
+ 
+    Lista* last = *head_ref; /* used in step 5*/
+ 
+    /* 2. put in the data */
+    new_node->valor = new_data;
+ 
+    /* 3. This new node is going to be the last node, so
+    make next of it as NULL*/
+    new_node->proximo = NULL;
+ 
+    /* 4. If the Linked List is empty, then make the new
+    * node as head */
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+ 
+    /* 5. Else traverse till the last node */
+    while (last->proximo != NULL)
+        last = last->proximo;
+ 
+    /* 6. Change the next of last node */
+    last->proximo = new_node;
+    return;
 }
 
 void imprimir(Lista *lista)
@@ -60,58 +89,70 @@ void imprimir(Lista *lista)
         printf("%d ", temp->valor);
         temp = temp->proximo;
     }
+    printf("\n");
 }
 
-/*
+
 void uniao(Lista **res, Lista *c1, Lista *c2)
 {
     Lista *temp1 = c1; Lista *temp2 = c2;
 
-    while(temp1 != NULL &&  temp2 != NULL) {
+    for(;;)
+    {
+        if(temp1 != NULL && temp2 == NULL) {
+            return;
+        }
+    
         if(temp1->valor < temp2->valor) {
-            inserir(res, temp1->valor);
-            temp1 = temp1->proximo;
+            inserir_ordenado(res, temp1->valor);
         }
-
         else if(temp2->valor < temp1->valor) {
-            inserir(res, temp2->valor);
-            temp2 = temp2->proximo;
-        }
-
-        else {
-            temp1 = temp1->proximo;
-            temp2 = temp2->proximo;
+            inserir_ordenado(res, temp2->valor);
         }
     }
+    
+    while(temp1 != NULL) {
+        inserir_ordenado(res, temp1->valor);
+        temp1= temp1->proximo;
+    }
+    
+    while(temp2 != NULL) {
+        inserir_ordenado(res, temp2->valor);
+        temp2 = temp2->proximo;
+    }
 }
-*/
+
+// 1 -> 3 -> 5
+// 2 -> 5 -> 6 -> 0
+
 
 
 int main(void)
 {
     Lista *c1 = NULL; Lista *c2 = NULL; Lista *resultado = NULL;
-    int n1, n2, valor;
-    
-    printf("Quantos elementos na lista 1? ");
-    scanf("%d", &n1);
-    while(getchar() != '\n');
-
-    printf("\nQuantos elementos na lista 2? ");
-    scanf("%d", &n2);
-    while(getchar() != '\n');
-
+    int n1 = 5, n2 = 5, valor;
+    int array1[5] = {6, 7, 1 ,23, 10};
+    int array2[5] = {1, 3, 5, 7, 9};
+  
     for(int i = 0; i < n1; i++) {
-        printf("\nDigite o %do elemento do conjuto 1: ", i + 1);
-        scanf("%d", &valor);
-        //inserir_ordenado(&c1, valor);
+        //printf("\nDigite o %do elemento do conjuto 1: ", i + 1);
+        //scanf("%d", &valor);
+        inserir_ordenado(&c1, array1[i]);
     }
 
     for(int i = 0; i < n2; i++) {
-        printf("\nDigite o %do elemento do conjuto 1: ", i + 1);
-        scanf("%d", &valor);
-        //inserir_ordenado(&c2, valor);
+        //printf("Digite o %do elemento do conjuto 1: ", i + 1);
+        //scanf("%d", &valor);
+        inserir_ordenado(&c2, array2[i]);
     }
+    
+    imprimir(c1);
+    printf("\n");
+    imprimir(c2);
 
+    uniao(&resultado, c1, c2);
+    printf("dihfidhfuihfiuhdsfudshf");
+    printf("Resultado\n");
     imprimir(resultado);
 
     return 0;
